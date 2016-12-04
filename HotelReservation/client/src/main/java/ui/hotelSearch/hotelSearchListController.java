67593165e579cde7532f2d83f0404addf7e977b8
guyifei <943045598@qq.com>
2016-12-04 11:53:30 +0800

    ，

--- HotelReservation/client/src/main/java/ui/hotelSearch/hotelSearchListController.java
+++ HotelReservation/client/src/main/java/ui/hotelSearch/hotelSearchListController.java
@@ -3,19 +3,51 @@ package ui.hotelSearch;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.Pagination;
+import javafx.scene.layout.VBox;
 import ui.UILaunch;
 
 public class hotelSearchListController implements Initializable{
 	private UILaunch application;
 	
+	@FXML
+	private Pagination pagination;
+	
+	private static int itemsPerPage=4;
+	
 	public void setApp(UILaunch application){
 		this.application= application;
+	}
+	
+	
+	
+	public VBox createPage(int pageIndex) {
+		VBox box = new VBox(5);
+		int page = pageIndex * itemsPerPage;
+		for (int i = page; i < page + itemsPerPage; i++) {
+			VBox element = new VBox();
+			Hyperlink link = new Hyperlink("Item " + (i+1));
+			link.setVisited(true);
+			Label text = new Label("Search results\nfor "+ link.getText());
+			element.getChildren().addAll(link, text);
+			box.getChildren().add(element);
+		}
+		return box;
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+		pagination.setStyle("-fx-border-color:blue;");
+		pagination.setPageFactory((Integer pageIndex) -> createPage(pageIndex));
+		
+		
+		
 		
 	}
 
--- HotelReservation/client/src/main/java/ui/webManagerGuide/webManagerGuideController.java
+++ HotelReservation/client/src/main/java/ui/webManagerGuide/webManagerGuideController.java
@@ -7,6 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.shape.Circle;
 import ui.UILaunch;
 
 public class webManagerGuideController implements Initializable{

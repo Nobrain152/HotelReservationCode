a35c8bed84e83216e9fa0f57685e14cd8738704d
txin15 <995472235@qq.com>
2016-12-16 21:15:21 +0800

    上传一个test以供测试

--- HotelReservation/client/src/test/java/vip/VipControllerTest.java
+++ HotelReservation/client/src/test/java/vip/VipControllerTest.java
@@ -10,11 +10,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 import bl.vipbl.Vip;
+import net.RMIManage;
 import dataservice.creditdataservice.CreditDataService;
 import dataservice.userdataservice.CustomerManagementDataService;
+import dataservice.userdataservice.UserManagementDataService;
 import dataservice.vipdataservice.VipDataService;
 import po.CommonVipPO;
 import po.LevelSystemPO;
+import util.DataServiceType;
 import util.ResultMsg;
 import util.VipType;
 import vo.CustomerInfoVO;
@@ -23,34 +26,41 @@ public class VipControllerTest {
 	int level;
 	int integral;
 	private VipDataService vipDataService;
-	private CustomerManagementDataService dataService;
+	private UserManagementDataService dataService;
 	private CreditDataService creditDataService;
+	Vip vip;
 	CustomerInfoVO customerInfoVO;
 	
 	@Before
 	public void setUp() throws Exception {
-		customerInfoVO = new CustomerInfoVO("10000001", "txin", "sfd", "18805156300", 100, true, VipType.COMMON_VIP);
+		vipDataService = (VipDataService)RMIManage.
+				getDataService(DataServiceType.VipDataService);
+		dataService = (UserManagementDataService)RMIManage.
+				getDataService(DataServiceType.UserManagementDataService);
+		creditDataService = (CreditDataService)RMIManage.
+				getDataService(DataServiceType.CreditDataService);
+		vip = new Vip(vipDataService,dataService,creditDataService);
+		customerInfoVO = new CustomerInfoVO("19954722", "ÌÆöÎ", "sfd", "18805156300", 300, true, VipType.COMMON_VIP);
 	}
+//
+//	@Test
+//	public void testSearchLevel() throws RemoteException {
+//		Vip vip = new Vip(vipDataService,dataService,creditDataService);
+//		level = vip.searchLevel(customerInfoVO);
+//		assertEquals(level,1);
+//	}
 
-	@Test
-	public void testSearchLevel() throws RemoteException {
-		Vip vip = new Vip(vipDataService,dataService,creditDataService);
-		level = vip.searchLevel(customerInfoVO);
-		assertEquals(level,1);
-	}
-
-	@Test
-	public void testRegisterVip() throws RemoteException {
-		Vip vip = new Vip(vipDataService,dataService,creditDataService);
-		vip.registerVip(customerInfoVO.getUsername(), customerInfoVO.getVipType(), "1997-05-13");
-		CommonVipPO commonVipPO = vipDataService.findByUserIDC(customerInfoVO.getUserID());
-		assertEquals(commonVipPO.getBirthday(), "1997-05-13");
-	}
+//	@Test
+//	public void testRegisterVip() throws RemoteException {
+//		vip.registerVip(customerInfoVO.getUserID(), customerInfoVO.getVipType(), "1997-05-13");
+//		CommonVipPO commonVipPO = vipDataService.findByUserIDC(customerInfoVO.getUserID());
+//		assertEquals(commonVipPO.getBirthday(), "1997-05-13");
+//	}
 	
 	@Test
 	public void testCreateLevelSystem() throws RemoteException{
-		
-		//LevelSystemPO levelSystemPO = new LevelSystemPO(levels, credits);
-		//ResultMsg resultMsg = vipDataService.insertL(levelSystemPO);
+		LevelSystemPO levelSystemPO = new LevelSystemPO(1, 100);
+		vipDataService.insertL(levelSystemPO);
+		assertEquals(levelSystemPO.getLevels(), 1);
 	}
 }

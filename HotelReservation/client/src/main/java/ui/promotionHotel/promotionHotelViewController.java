82ded1d49080a906c269e7bc4312f59addfbb287
guyifei <943045598@qq.com>
2016-12-18 22:33:05 +0800

    促销策略初始化

--- HotelReservation/client/src/main/java/ui/promotionHotel/promotionHotelViewController.java
+++ HotelReservation/client/src/main/java/ui/promotionHotel/promotionHotelViewController.java
@@ -1,21 +1,31 @@
 package ui.promotionHotel;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import bl.userbl.HotelStuffHotelOperationController;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import ui.UILaunch;
+import util.PromotionHotelType;
+import vo.PromotionHotelVO;
 
 public class promotionHotelViewController implements Initializable {
 	private UILaunch application;
+	private HotelStuffHotelOperationController promotionManage;
 
 	// 会员生日优惠策略
 	@FXML
@@ -27,6 +37,14 @@ public class promotionHotelViewController implements Initializable {
 	private ObservableList<PromotionBirthday> data_birthday;
 	@FXML
 	private Button btn_birthday_modify;
+	@FXML
+	private Button btn_birthday_add;
+	@FXML
+	private Button btn_birthday_delete;
+	@FXML
+	private TextField tf_birthday_level;
+	@FXML
+	private TextField tf_birthday_discount;
 
 	// 三间以上优惠策略
 	@FXML
@@ -38,6 +56,14 @@ public class promotionHotelViewController implements Initializable {
 	private ObservableList<PromotionThreeRoom> data_threeroom;
 	@FXML
 	private Button btn_threeroom_modify;
+	@FXML
+	private Button btn_threeroom_add;
+	@FXML
+	private Button btn_threeroom_delete;
+	@FXML
+	private TextField tf_threeroom_number;
+	@FXML
+	private TextField tf_threeroom_discount;
 
 	// 合作企业优惠策略
 	@FXML
@@ -49,6 +75,14 @@ public class promotionHotelViewController implements Initializable {
 	private ObservableList<PromotionEnterprise> data_enterprise;
 	@FXML
 	private Button btn_enterprise_modify;
+	@FXML
+	private Button btn_enterprise_add;
+	@FXML
+	private Button btn_enterprise_delete;
+	@FXML
+	private TextField tf_enterprise_name;
+	@FXML
+	private TextField tf_enterprise_discount;
 
 	// 自定义优惠策略
 	@FXML
@@ -62,6 +96,16 @@ public class promotionHotelViewController implements Initializable {
 	private ObservableList<PromotionDiy> data_diy;
 	@FXML
 	private Button btn_diy_modify;
+	@FXML
+	private Button btn_diy_add;
+	@FXML
+	private Button btn_diy_delete;
+	@FXML
+	private DatePicker dp_diy_start;
+	@FXML
+	private DatePicker dp_diy_end;
+	@FXML
+	private TextField tf_diy_discount;
 	
 
 	@FXML
@@ -82,8 +126,28 @@ public class promotionHotelViewController implements Initializable {
 	}
 	
 	@FXML
+	public void btn_birthday_addAction(ActionEvent ev) {
+		
+	}
+	
+	@FXML
+	public void btn_birthday_deleteAction(ActionEvent ev) {
+		
+	}
+	
+	@FXML
 	public void btn_diy_modifyAction(ActionEvent ev) {
 		application.gotopromotionHotelDIY();
+	}
+	
+	@FXML
+	public void btn_diy_addAction(ActionEvent ev) {
+		
+	}
+	
+	@FXML
+	public void btn_diy_deleteAction(ActionEvent ev) {
+		
 	}
 	
 	@FXML
@@ -92,49 +156,126 @@ public class promotionHotelViewController implements Initializable {
 	}
 	
 	@FXML
+	public void btn_enterprise_addAction(ActionEvent ev) {
+		
+	}
+	
+	@FXML
+	public void btn_enterprise_deleteAction(ActionEvent ev) {
+		
+	}
+	
+	@FXML
 	public void btn_threeroom_modifyAction(ActionEvent ev) {
 		application.gotopromotionHotelRoom();
+	}
+	
+	@FXML
+	public void btn_threeroom_addAction(ActionEvent ev) {
+		
+	}
+	
+	@FXML
+	public void btn_threeroom_deleteAction(ActionEvent ev) {
+		
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+		promotionManage=new HotelStuffHotelOperationController();
+		
+		PromotionHotelVO vo_birthday=new PromotionHotelVO();
+		vo_birthday.setType(PromotionHotelType.BIRTH_PROMOTION);
+		ArrayList<PromotionHotelVO> promotion_birthdayList=promotionManage.HotelPromotionInquire(vo_birthday);
+		ArrayList<PromotionBirthday> data_birthdayList=new ArrayList<PromotionBirthday>();
+		int size_birthday=promotion_birthdayList.size();
+		for(int i=0;i<size_birthday;i++){
+			PromotionHotelVO tempPromotionVO=promotion_birthdayList.get(i);
+			data_birthdayList.add(new PromotionBirthday(tempPromotionVO.getLevel(),tempPromotionVO.getRatio()));
+		}
+		data_birthday=FXCollections.observableArrayList(data_birthdayList);		
+		promotion_birthday_level.setCellValueFactory(new PropertyValueFactory<>("level"));
+		promotion_birthday_discount.setCellValueFactory(new PropertyValueFactory<>("discount"));
+		promotion_birthday.setItems(data_birthday);
+		
+		PromotionHotelVO vo_threeroom=new PromotionHotelVO();
+		vo_threeroom.setType(PromotionHotelType.OVERTHREE_PROMOTION);
+		ArrayList<PromotionHotelVO> promotion_threeroomList=promotionManage.HotelPromotionInquire(vo_threeroom);
+		ArrayList<PromotionThreeRoom> data_threeroomList=new ArrayList<PromotionThreeRoom>();
+		int size_threeroom=promotion_threeroomList.size();
+		for(int i=0;i<size_threeroom;i++){
+			PromotionHotelVO tempPromotionVO=promotion_threeroomList.get(i);
+			data_threeroomList.add(new PromotionThreeRoom(tempPromotionVO.getNumber(),tempPromotionVO.getRatio()));
+		}
+		data_threeroom=FXCollections.observableArrayList(data_threeroomList);		
+		promotion_threeroom_number.setCellValueFactory(new PropertyValueFactory<>("number"));
+		promotion_threeroom_discount.setCellValueFactory(new PropertyValueFactory<>("discount"));
+		promotion_threeroom.setItems(data_threeroom);
+		
+		PromotionHotelVO vo_enterprise=new PromotionHotelVO();
+		vo_enterprise.setType(PromotionHotelType.JOIN_PROMOTION);
+		ArrayList<PromotionHotelVO> promotion_enterpriseList=promotionManage.HotelPromotionInquire(vo_enterprise);
+		ArrayList<PromotionEnterprise> data_enterpriseList=new ArrayList<PromotionEnterprise>();
+		int size_enterprise=promotion_enterpriseList.size();
+		for(int i=0;i<size_enterprise;i++){
+			PromotionHotelVO tempPromotionVO=promotion_enterpriseList.get(i);
+			data_enterpriseList.add(new PromotionEnterprise(tempPromotionVO.getBusinessName(),tempPromotionVO.getRatio()));
+		}
+		data_enterprise=FXCollections.observableArrayList(data_enterpriseList);		
+		promotion_enterprise_name.setCellValueFactory(new PropertyValueFactory<>("name"));
+		promotion_enterprise_discount.setCellValueFactory(new PropertyValueFactory<>("discount"));
+		promotion_enterprise.setItems(data_enterprise);
 
+		PromotionHotelVO vo_diy=new PromotionHotelVO();
+		vo_diy.setType(PromotionHotelType.HOTEL_CUSTOM_PROMOTION);
+		ArrayList<PromotionHotelVO> promotion_diyList=promotionManage.HotelPromotionInquire(vo_diy);
+		ArrayList<PromotionDiy> data_diyList=new ArrayList<PromotionDiy>();
+		int size_diy=promotion_diyList.size();
+		for(int i=0;i<size_diy;i++){
+			PromotionHotelVO tempPromotionVO=promotion_diyList.get(i);
+			data_diyList.add(new PromotionDiy(tempPromotionVO.getTimeBegin(),tempPromotionVO.getTimeOver(),tempPromotionVO.getRatio()));
+		}
+		data_diy=FXCollections.observableArrayList(data_diyList);		
+		promotion_diy_startTime.setCellValueFactory(new PropertyValueFactory<>("startTime"));
+		promotion_diy_endTime.setCellValueFactory(new PropertyValueFactory<>("endTime"));
+		promotion_diy_discount.setCellValueFactory(new PropertyValueFactory<>("discount"));
+		promotion_diy.setItems(data_diy);
 	}
 	
 	public static class PromotionBirthday {
-		private SimpleStringProperty level;
-		private SimpleStringProperty discount;
+		private SimpleIntegerProperty level;
+		private SimpleDoubleProperty discount;
 		
-		private PromotionBirthday(String level,String discount){
-			this.level=new SimpleStringProperty(level);
-			this.discount=new SimpleStringProperty(discount);
+		private PromotionBirthday(int level,double discount){
+			this.level=new SimpleIntegerProperty(level);
+			this.discount=new SimpleDoubleProperty(discount);
 		}
 		
-		public String getLevel(){
+		public int getLevel(){
 			return level.get();
 		}
 		
-		public void setLevel(String str){
-			level.set(str);
+		public void setLevel(int n){
+			level.set(n);
 		}
 		
-		public String getDiscount(){
+		public double getDiscount(){
 			return discount.get();
 		}
 		
-		public void setDiscount(String str){
-			discount.set(str);
+		public void setDiscount(double n){
+			discount.set(n);
 		}
 	}
 	
 	public static class PromotionThreeRoom {
 		private SimpleIntegerProperty number;
-		private SimpleStringProperty discount;
+		private SimpleDoubleProperty discount;
 		
-		private PromotionThreeRoom(int number,String discount){
+		private PromotionThreeRoom(int number,double discount){
 			this.number=new SimpleIntegerProperty(number);
-			this.discount=new SimpleStringProperty(discount);
+			this.discount=new SimpleDoubleProperty(discount);
 		}
 		
 		public int getNumber(){
@@ -145,22 +286,22 @@ public class promotionHotelViewController implements Initializable {
 			number.set(n);
 		}
 		
-		public String getDiscount(){
+		public double getDiscount(){
 			return discount.get();
 		}
 		
-		public void setDiscount(String str){
-			discount.set(str);
+		public void setDiscount(double n){
+			discount.set(n);
 		}
 	}
 	
 	public static class PromotionEnterprise {
 		private SimpleStringProperty name;
-		private SimpleStringProperty discount;
+		private SimpleDoubleProperty discount;
 		
-		private PromotionEnterprise(String name,String discount){
+		private PromotionEnterprise(String name,double discount){
 			this.name=new SimpleStringProperty(name);
-			this.discount=new SimpleStringProperty(discount);
+			this.discount=new SimpleDoubleProperty(discount);
 		}
 		
 		public String getName(){
@@ -171,24 +312,24 @@ public class promotionHotelViewController implements Initializable {
 			name.set(str);
 		}
 		
-		public String getDiscount(){
+		public double getDiscount(){
 			return discount.get();
 		}
 		
-		public void setDiscount(String str){
-			discount.set(str);
+		public void setDiscount(double n){
+			discount.set(n);
 		}
 	}
 	
 	public static class PromotionDiy{
 		private SimpleStringProperty startTime;
 		private SimpleStringProperty endTime;
-		private SimpleStringProperty discount;
+		private SimpleDoubleProperty discount;
 		
-		private PromotionDiy(String startTime,String endTime,String discount){
+		private PromotionDiy(String startTime,String endTime,double discount){
 			this.startTime=new SimpleStringProperty(startTime);
 			this.endTime=new SimpleStringProperty(endTime);
-			this.discount=new SimpleStringProperty(discount);
+			this.discount=new SimpleDoubleProperty(discount);
 		}
 		
 		public String getStartTime(){
@@ -207,12 +348,12 @@ public class promotionHotelViewController implements Initializable {
 			endTime.set(str);
 		}
 		
-		public String getDiscount(){
+		public double getDiscount(){
 			return discount.get();
 		}
 		
-		public void setDiscount(String str){
-			discount.set(str);
+		public void setDiscount(double n){
+			discount.set(n);
 		}
 	}
 
--- HotelReservation/client/src/main/java/ui/promotionWeb/promotionWebViewController.java
+++ HotelReservation/client/src/main/java/ui/promotionWeb/promotionWebViewController.java
@@ -1,20 +1,32 @@
 package ui.promotionWeb;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import bl.userbl.WebStuffWebsiteManagementController;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import ui.UILaunch;
+import util.Area;
+import util.PromotionWebType;
+import vo.PromotionWebVO;
 
 public class promotionWebViewController implements Initializable {
 	private UILaunch application;
+	WebStuffWebsiteManagementController promotionManage;
 
 	// 会员等级优惠策略
 	@FXML
@@ -26,6 +38,14 @@ public class promotionWebViewController implements Initializable {
 	private ObservableList<PromotionVIP> data_vip;
 	@FXML
 	private Button btn_vip_modify;
+	@FXML
+	private Button btn_vip_add;
+	@FXML
+	private Button btn_vip_delete;
+	@FXML
+	private TextField tf_vip_level;
+	@FXML
+	private TextField tf_vip_discount;
 
 	// 特定商圈优惠策略
 	@FXML
@@ -37,6 +57,14 @@ public class promotionWebViewController implements Initializable {
 	private ObservableList<PromotionArea> data_area;
 	@FXML
 	private Button btn_area_modify;
+	@FXML
+	private Button btn_area_add;
+	@FXML
+	private Button btn_area_delete;
+	@FXML
+	private TextField tf_area_name;
+	@FXML
+	private TextField tf_area_discount;
 
 	// 自定义优惠策略
 	@FXML
@@ -50,6 +78,16 @@ public class promotionWebViewController implements Initializable {
 	private ObservableList<PromotionDiy> data_diy;
 	@FXML
 	private Button btn_diy_modify;
+	@FXML
+	private Button btn_diy_add;
+	@FXML
+	private Button btn_diy_delete;
+	@FXML
+	private DatePicker dp_diy_start;
+	@FXML
+	private DatePicker dp_diy_end;
+	@FXML
+	private TextField tf_diy_discount;
 
 	@FXML
 	private Button btn_Cancel;
@@ -60,7 +98,18 @@ public class promotionWebViewController implements Initializable {
 	
 	@FXML
 	public void btn_vip_modifyAction(ActionEvent ev) {
+		
 		application.gotopromotionWebVIP();
+	}
+	
+	@FXML
+	public void btn_vip_addAction(ActionEvent ev) {
+		
+	}
+	
+	@FXML
+	public void btn_vip_deleteAction(ActionEvent ev) {
+		
 	}
 	
 	@FXML
@@ -69,8 +118,28 @@ public class promotionWebViewController implements Initializable {
 	}
 	
 	@FXML
+	public void btn_area_addAction(ActionEvent ev) {
+		
+	}
+	
+	@FXML
+	public void btn_area_deleteAction(ActionEvent ev) {
+		
+	}
+	
+	@FXML
 	public void btn_diy_modifyAction(ActionEvent ev) {
 		application.gotopromotionWebDIY();
+	}
+	
+	@FXML
+	public void btn_diy_addAction(ActionEvent ev) {
+		
+	}
+	
+	@FXML
+	public void btn_diy_deleteAction(ActionEvent ev) {
+		
 	}
 
 	@FXML
@@ -81,42 +150,93 @@ public class promotionWebViewController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-
+		promotionManage=new WebStuffWebsiteManagementController();
+		
+		PromotionWebVO vo_vip=new PromotionWebVO();
+		vo_vip.setType(PromotionWebType.VIP_LEVEL_PROMOTION);
+		ArrayList<PromotionWebVO> promotion_vipList=promotionManage.WebsiteStrategeInquire(vo_vip);
+		ArrayList<PromotionVIP> data_vipList=new ArrayList<PromotionVIP>();
+		int size_vip=promotion_vipList.size();
+		for(int i=0;i<size_vip;i++){
+			PromotionWebVO tempPromotionVO=promotion_vipList.get(i);
+			data_vipList.add(new PromotionVIP(tempPromotionVO.getLevel(),tempPromotionVO.getRatio()));
+		}
+		data_vip=FXCollections.observableArrayList(data_vipList);		
+		promotion_vip_level.setCellValueFactory(new PropertyValueFactory<>("level"));
+		promotion_vip_discount.setCellValueFactory(new PropertyValueFactory<>("discount"));
+		promotion_vip.setItems(data_vip);
+		
+		PromotionWebVO vo_area=new PromotionWebVO();
+		vo_area.setType(PromotionWebType.VIP_CIRCLE_PROMOTION);
+		ArrayList<PromotionWebVO> promotion_areaList=promotionManage.WebsiteStrategeInquire(vo_area);
+		ArrayList<PromotionArea> data_areaList=new ArrayList<PromotionArea>();
+		int size_area=promotion_areaList.size();
+		for(int i=0;i<size_area;i++){
+			PromotionWebVO tempPromotionVO=promotion_areaList.get(i);
+			Area area=tempPromotionVO.getLocation();
+			String areaStr=null;			
+			switch(area){
+			case EAST: areaStr="东区";break;
+			case WEST: areaStr="西区";break;
+			case SOUTH: areaStr="南区";break;
+			case NORTH: areaStr="北区";
+			}
+			data_areaList.add(new PromotionArea(areaStr,tempPromotionVO.getRatio()));
+		}
+		data_area=FXCollections.observableArrayList(data_areaList);		
+		promotion_area_name.setCellValueFactory(new PropertyValueFactory<>("name"));
+		promotion_area_discount.setCellValueFactory(new PropertyValueFactory<>("discount"));
+		promotion_area.setItems(data_area);
+		
+		PromotionWebVO vo_diy=new PromotionWebVO();
+		vo_diy.setType(PromotionWebType.WEB_CUSTOM_PROMOTION);
+		ArrayList<PromotionWebVO> promotion_diyList=promotionManage.WebsiteStrategeInquire(vo_diy);
+		ArrayList<PromotionDiy> data_diyList=new ArrayList<PromotionDiy>();
+		int size_diy=promotion_diyList.size();
+		for(int i=0;i<size_diy;i++){
+			PromotionWebVO tempPromotionVO=promotion_diyList.get(i);
+			data_diyList.add(new PromotionDiy(tempPromotionVO.getTimeBegin(),tempPromotionVO.getTimeOver(),tempPromotionVO.getRatio()));
+		}
+		data_diy=FXCollections.observableArrayList(data_diyList);		
+		promotion_diy_startTime.setCellValueFactory(new PropertyValueFactory<>("startTime"));
+		promotion_diy_endTime.setCellValueFactory(new PropertyValueFactory<>("endTime"));
+		promotion_diy_discount.setCellValueFactory(new PropertyValueFactory<>("discount"));
+		promotion_diy.setItems(data_diy);
 	}
 	
 	public static class PromotionVIP{
-		private SimpleStringProperty level;
-		private SimpleStringProperty discount;
+		private SimpleIntegerProperty level;
+		private SimpleDoubleProperty discount;
 		
-		private PromotionVIP(String level,String discount){
-			this.level=new SimpleStringProperty(level);
-			this.discount=new SimpleStringProperty(discount);
+		private PromotionVIP(int level,double discount){
+			this.level=new SimpleIntegerProperty(level);
+			this.discount=new SimpleDoubleProperty(discount);
 		}
 		
-		public String getLevel(){
+		public int getLevel(){
 			return level.get();
 		}
 		
-		public void setLevel(String str){
-			level.set(str);
+		public void setLevel(int n){
+			level.set(n);
 		}
 		
-		public String getDiscount(){
+		public double getDiscount(){
 			return discount.get();
 		}
 		
-		public void setDiscount(String str){
-			discount.set(str);
+		public void setDiscount(double n){
+			discount.set(n);
 		}
 	}
 	
 	public static class PromotionArea {
 		private SimpleStringProperty name;
-		private SimpleStringProperty discount;
+		private SimpleDoubleProperty discount;
 		
-		private PromotionArea(String name,String discount){
+		private PromotionArea(String name,double discount){
 			this.name=new SimpleStringProperty(name);
-			this.discount=new SimpleStringProperty(discount);
+			this.discount=new SimpleDoubleProperty(discount);
 		}
 		
 		public String getName(){
@@ -127,24 +247,24 @@ public class promotionWebViewController implements Initializable {
 			name.set(str);
 		}
 		
-		public String getDiscount(){
+		public double getDiscount(){
 			return discount.get();
 		}
 		
-		public void setDiscount(String str){
-			discount.set(str);
+		public void setDiscount(double n){
+			discount.set(n);
 		}
 	}
 	
 	public static class PromotionDiy{
 		private SimpleStringProperty startTime;
 		private SimpleStringProperty endTime;
-		private SimpleStringProperty discount;
+		private SimpleDoubleProperty discount;
 		
-		private PromotionDiy(String startTime,String endTime,String discount){
+		private PromotionDiy(String startTime,String endTime,double discount){
 			this.startTime=new SimpleStringProperty(startTime);
 			this.endTime=new SimpleStringProperty(endTime);
-			this.discount=new SimpleStringProperty(discount);
+			this.discount=new SimpleDoubleProperty(discount);
 		}
 		
 		public String getStartTime(){
@@ -163,12 +283,12 @@ public class promotionWebViewController implements Initializable {
 			endTime.set(str);
 		}
 		
-		public String getDiscount(){
+		public double getDiscount(){
 			return discount.get();
 		}
 		
-		public void setDiscount(String str){
-			discount.set(str);
+		public void setDiscount(double n){
+			discount.set(n);
 		}
 	
 	}

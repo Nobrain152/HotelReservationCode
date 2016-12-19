6283f4a5777333b8be739de18404f248b351906b
没脑子 <814335296@qq.com>
2016-12-19 23:44:06 +0800

    数据层

--- HotelReservation/server/src/test/java/data/PromotionHotelDataServiceImplTest.java
+++ HotelReservation/server/src/test/java/data/PromotionHotelDataServiceImplTest.java
@@ -2,18 +2,31 @@ package data;
 
 import static org.junit.Assert.*;
 
+import java.rmi.RemoteException;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import data.promotiondata.PromotionHotelDataServiceImpl;
+import po.PromotionHotelPO;
+import util.PromotionHotelType;
+import util.ResultMsg;
+
 public class PromotionHotelDataServiceImplTest {
 
+	PromotionHotelDataServiceImpl impl;
+	PromotionHotelPO po;
+	
 	@Before
 	public void setUp() throws Exception {
+		impl = new PromotionHotelDataServiceImpl();
+		po = new PromotionHotelPO(PromotionHotelType.OVERTHREE_PROMOTION, "5004", 3, 2.33);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInsert() throws RemoteException {
+		ResultMsg aMsg = impl.insert(po);
+		assertEquals(aMsg, ResultMsg.SUCCESS);
 	}
 
 }

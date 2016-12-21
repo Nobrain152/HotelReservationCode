5dddc0e5142a167312754f211dae73b2553a028e
C1122C <763443070@qq.com>
2016-12-21 19:39:15 +0800

    改了一点酒店的代码

--- HotelReservation/client/src/test/java/hotel/HotelInfoMaintainControllerTest.java
+++ HotelReservation/client/src/test/java/hotel/HotelInfoMaintainControllerTest.java
@@ -26,11 +26,11 @@ public class HotelInfoMaintainControllerTest {
 	@Before
 	public void setUp() {
 		hotel1=new HotelInfoVO("StarHotel",Adress.NANJING,Area.EAST,5,"It's a wonderful hotel","cafe, garden and so on",
-				true,"431543263",1.1,120);
+				true,"5005",66.66,100);
 		hotel2=new HotelInfoVO("SunHotel",Adress.NANJING,Area.WEST,4,"It's a nice hotel","cafe",
-				false,"452346236",1.1,120);
-		hotel3=new HotelInfoVO("MoonHotel",Adress.NANJING,Area.NORTH,-1,"It's a terrible hotel","nothing",
-				true,"234655754634",1.1,120);
+				false,"5002",77.77,100);
+		hotel3=new HotelInfoVO("MoonHotel1",Adress.NANJING,Area.NORTH,2,"It's a terrible hotel","nothing",
+				true,"234655754634",80.01,120);
 				
 				
 		impl = new HotelInfoMaintainController();
@@ -50,10 +50,10 @@ public class HotelInfoMaintainControllerTest {
     }
 	
 	@Test
-	public void testcheckInfo(){
-		ResultMsg msg = impl.checkInfo(hotel1); 
+	public void addHotel(){
+		String msg = impl.addHotel(hotel3); 
 		//assertEquals(msg.getMessage(), reMsg.getMessage());
-		assertEquals(msg, reMsg);
+		assertEquals(msg,"5012");
     }
 
 

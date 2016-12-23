5d0461e61b99d04f81a464fc70dafa335bc673da
guyifei <943045598@qq.com>
2016-12-23 18:41:32 +0800

    小改动

--- HotelReservation/client/src/main/java/ui/hotelReserve/hotelReserveViewController.java
+++ HotelReservation/client/src/main/java/ui/hotelReserve/hotelReserveViewController.java
@@ -3,14 +3,25 @@ package ui.hotelReserve;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import ui.UILaunch;
 
 public class hotelReserveViewController implements Initializable{
 	private UILaunch application;
 	
+	@FXML
+	private Button btn_Cancel;
+	
 	public void setApp(UILaunch application){
 		this.application= application;
+	}
+	
+	@FXML
+	public void btn_CancelAction(ActionEvent event){
+		application.gotohotelInfoSearched();
 	}
 	
 
